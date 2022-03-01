bb04194c0fb806d77f4c171461a2c3e5e6e0402c
mdAlnahian <muhammedalnahian@gmail.com>
2022-03-01 16:22:00 +0600

    hdfu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 // js started from here
+//show all button
+const showAllButton = (displayStyle) => {
+  const showAll = document.getElementById("btn-to-show-all");
+  showAll.style.display = displayStyle;
+};
+showAllButton("none");
 
 //search input button function
 const getPhoneInput = () => {
   const searchFeild = document.getElementById("searchFeild");
   const searchText = searchFeild.value;
   //error displaying task
+
   const err = document.getElementById("err1");
   if (searchText === "") {
     err.innerText = `Sorry No result Matched😥`;
@@ -17,7 +24,6 @@ const getPhoneInput = () => {
   }
 };
 // getPhoneInput();
-
 const getTheDetailsOfPhones = (phones) => {
   //   console.log(phones)
   const err = document.getElementById("err2");
@@ -25,19 +31,18 @@ const getTheDetailsOfPhones = (phones) => {
     err.innerText = `Sorry No result Matched😥`;
   } else {
     err.innerText = `${phones.length} result found`;
-
-    //testing
-          const container = document.getElementById("container");
-          container.textContent = "";
-              phones.forEach((phone) => {
-                // console.log(phones.length)
-                // console.log(phone)
-                const div = document.createElement("div");
-                div.classList.add("element");
-                div.innerHTML = `
+    const container = document.getElementById("container");
+    // console.log(phones);
+    container.textContent = "";
+    phones.slice(0, 3).forEach((phone) => {
+      // console.log(phones.length)
+      // console.log(phone)
+      const div = document.createElement("div");
+      div.classList.add("element");
+      div.innerHTML = `
                      <div class="col shadow-lg rounded">
                         <div class="card p-4">
-                        <img src="${phone.image}" class="card-img-top phone-image" alt="...">
+                        <img src="${phone.image}" class="card-img-top phone-image" alt="">
                         <div class="card-body">
                             <h5 class="card-title">${phone.brand}</h5>
                             <h4 class="card-text">${phone.phone_name}</h4>
@@ -45,17 +50,17 @@ const getTheDetailsOfPhones = (phones) => {
                             <button onclick="explorePhoneDetails('${phone.slug}')" type="button" class="btn btn-success">Explore</button>
                         </div>
                         </div>
-                      
                         </div>
                     </div>
         
         `;
-                container.appendChild(div);
-              });
+      container.appendChild(div);
+    });
 
+    showAllButton("block");
   }
 };
-
+// MORE details of every clickable single elements
 const explorePhoneDetails = (slug) => {
   //   console.log(slug);
   const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
@@ -66,13 +71,13 @@ const explorePhoneDetails = (slug) => {
 };
 
 const moreInfoContainer = (details) => {
-  console.log(details);
+  // console.log(details);
   const moreInfoContainer = document.getElementById("more-info-container");
   moreInfoContainer.textContent = "";
   const div = document.createElement("div");
   div.innerHTML = `
     
-            <div class="card mb-3 mx-auto p-3" style="max-width: 640px;">
+            <div class="card mb-3 mx-auto p-3 shadow-lg" style="max-width: 640px;">
         <div class="row g-0">
             <div class="col-md-4 my-auto">
             <img src="${details.image}"class="img-fluid rounded-start" alt="">
@@ -97,4 +102,47 @@ const moreInfoContainer = (details) => {
     
     `;
   moreInfoContainer.appendChild(div);
+};
+
+// all data loaded by Show All button click
+const lastOne = () => {
+  const searchFeild = document.getElementById("searchFeild");
+  const searchText = searchFeild.value;
+  const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}  `;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => allResult(data.data));
+};
+
+// lastOne();
+
+const allResult = (phones) => {
+  //   console.log(phones)
+  const container = document.getElementById("container");
+  container.textContent = "";
+  phones.forEach((phone) => {
+    // console.log(phones.length)
+    // console.log(phone)
+    const div = document.createElement("div");
+    div.classList.add("element");
+    div.innerHTML = `
+                     <div class="col shadow-lg rounded">
+                        <div class="card p-4">
+                        <img src="${phone.image}" class="card-img-top phone-image" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">${phone.brand}</h5>
+                            <h4 class="card-text">${phone.phone_name}</h4>
+                          <div>
+                            <button onclick="explorePhoneDetails('${phone.slug}')" type="button" class="btn btn-success">Explore</button>
+                        </div>
+                        </div>
+                      
+                        </div>
+                    </div>
+        
+        `;
+    container.appendChild(div);
+  });
+
+  showAllButton("none");
 };
